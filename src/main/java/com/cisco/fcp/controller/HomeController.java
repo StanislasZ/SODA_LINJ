48db0a0633b0f7607e4a3a153b5c1f12b4cd5989
ruozhao <ruozhao@cisco.com>
2018-11-16 19:43:29 +0800

    init commit

--- src/main/java/com/cisco/fcp/controller/HomeController.java
+++ src/main/java/com/cisco/fcp/controller/HomeController.java
@@ -1,6 +1,6 @@
 package com.cisco.fcp.controller;
 
-import com.cisco.fcp.entity.FieldForSimulator;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
